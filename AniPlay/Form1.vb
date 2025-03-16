6193a74a8349fb70cbccfe00b5eb7ede651d3a21
rahehehehehexd <dizonisaacrossdale@gmail.com>
2025-03-16 17:46:36 +0800

    Register Design Done

--- AniPlay/Form1.vb
+++ AniPlay/Form1.vb
@@ -1,5 +1,17 @@
-﻿Public Class Form1
+﻿Public Class frmRegister
     Private Sub Form1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
 
     End Sub
 End Class
