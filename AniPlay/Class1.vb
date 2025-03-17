6c56afd8c3b7986e930b01bd3f7cad9c1bedcbe0
Kawtsun <morpheusjoshua71@gmail.com>
2025-03-17 17:55:52 +0800

    Create user class

---
+++ AniPlay/Class1.vb
@@ -0,0 +1,7 @@
+﻿Public Class User
+    Public Property name As String
+    Public Property email As String
+    Public Property username As String
+    Public Property password As String
+
+End Class
