5af482ac281b26ea34eab09722a6990953554409
Kawtsun <morpheusjoshua71@gmail.com>
2025-03-17 18:22:05 +0800

    Error validation in checking users if empty

--- AniPlay/frmLogin.vb
+++ AniPlay/frmLogin.vb
@@ -2,6 +2,13 @@
     Public RegisteredUsers As List(Of User)
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        ' Check if there are no registered users
+        If RegisteredUsers Is Nothing OrElse RegisteredUsers.Count = 0 Then
+            MessageBox.Show("No users have been registered yet. Please register first.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        ' Proceed with the login process
         Dim username As String = txtUsername.Text
         Dim password As String = txtPassword.Text
 
@@ -13,6 +20,7 @@
             MessageBox.Show("Invalid username or password.")
         End If
     End Sub
+
 
     Private Sub lnkRegister_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkRegister.LinkClicked
         Me.Hide()
